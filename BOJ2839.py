291749eb8d706d2cd3b0b8d8351a460b63c1ef7b
HAESU-JEONG <junghaesu7@gmail.com>
2022-07-28 14:04:43 +0900

    백준 2839 풀이

---
+++ BOJ2839.py
@@ -0,0 +1,12 @@
+n = int(input())
+cnt = 0
+
+while n >= 0:
+    if n % 5 == 0:
+        cnt = cnt + (n // 5)
+        print(cnt)
+        break
+    n = n - 3
+    cnt = cnt + 1
+else:
+    print(-1)
