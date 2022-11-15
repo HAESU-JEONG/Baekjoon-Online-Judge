e4a99c886443cceecc70013bfa00d513ab00e8ce
HAESU-JEONG <junghaesu7@gmail.com>
2022-11-15 10:18:33 +0900

    백준 2501 풀이

---
+++ BOJ2501.py
@@ -0,0 +1,11 @@
+n, k = map(int, input().split())
+result = 0
+
+for i in range(1, n+1):
+    if n % i == 0:
+        k -= 1
+        if k == 0:
+            result = i
+            break
+        
+print(result)
