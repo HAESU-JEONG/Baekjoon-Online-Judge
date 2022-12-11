03828d922d35ae39aef6f962a381b66cc1a8b033
HAESU-JEONG <junghaesu7@gmail.com>
2022-12-11 09:04:17 +0900

    백준 8974 풀이

---
+++ BOJ8974.py
@@ -0,0 +1,11 @@
+a, b = map(int, input().split())
+num_list = [0]
+for i in range(1, b+1):
+    for j in range(i):
+        num_list.append(i)
+
+result = 0
+for i in range(a, b+1):
+    result += num_list[i]
+
+print(result)
